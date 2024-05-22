f2a0dd46143189de3bd9f41bfa193ed5f8b45421
Mohammed Hamid <154968967+Mo2Hammed2023@users.noreply.github.com>
2024-05-22 14:52:32 +0300

    Add files via upload

---
+++ Creat_and_deal_with_arrays.java
@@ -0,0 +1,98 @@
+package creat_and_deal_with_arrays;
+
+import java.util.Scanner;
+
+public class Creat_and_deal_with_arrays {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter The Size Of Array : ");
+        int size = in.nextInt();
+        int array[][] = new int[size][size];
+        int num_of_operation;
+        int frist_try = 1;
+        do {
+            System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+            System.out.println("1)Enter Array Elemens");
+            System.out.println("2)Print Array Elemens");
+            System.out.println("3)Search  In Array");
+            System.out.println("4)Update Array Elemens");
+            System.out.println("5)Exit");
+            System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+            System.out.print("Enter The Number of Thr Operation : ");
+            num_of_operation = in.nextInt();
+            if (num_of_operation == 1 || frist_try > 1) {
+                switch (num_of_operation) {
+                    case 1: {
+                        System.out.println("Enter The Elements Of Array : ");
+                        for (int row = 0; row < array.length; row++) {
+                            for (int column = 0; column < array.length; column++) {
+                                array[row][column] = in.nextInt();
+                            }
+                        }
+                        break;
+                    }
+                    case 2: {
+                        System.out.println("The Elements Of Array : ");
+                        for (int row = 0; row < array.length; row++) {
+                            for (int column = 0; column < array.length; column++) {
+                                System.out.print(array[row][column] + " ");
+                            }
+                            System.out.println(" ");
+                        }
+                        break;
+                    }
+
+                    case 3: {
+                        int row = 0, column = 0;
+                        System.out.println("Enter The Number That You Want To Found It : ");
+                        int num = in.nextInt();
+                        boolean f = false;
+                        for (int row_2 = 0; row_2 < array.length; row_2++) {
+                            for (int column_2 = 0; column_2 < array.length; column_2++) {
+                                if (num == array[row_2][column_2]) {
+                                    f = true;
+                                    row = row_2;
+                                    column = column_2;
+                                }
+                            }
+                        }
+                        if (f) {
+                            System.out.println("Found");
+                            System.out.println("The Number " + num + " Is Located In Row Number " + (row + 1) + " And In The Column Number " + (column + 1) + " .");
+                        } else {
+                            System.out.println("No Found");
+                        }
+                        break;
+                    }
+
+                    case 4: {
+                        int row = 0, column = 0;
+                        System.out.print("Enter The Number That You Want To Change It : ");
+                        int num = in.nextInt();
+                        System.out.println();
+                        int num1;
+                        boolean f = false;
+                        for (int row_2 = 0; row_2 < array.length; row_2++) {
+                            for (int column_2 = 0; column_2 < array.length; column_2++) {
+                                if (num == array[row_2][column_2]) {
+                                    System.out.print("Enter The Number That You Want To Put It Instead Of Number " + num + " : ");
+                                    num1 = in.nextInt();
+                                    array[row_2][column_2] = num1;
+                                    f = true;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+
+                    default:
+                        System.out.println("There Is No Option ");
+                }
+            } else {
+                System.out.println("You Should To Enter Number One");
+            }
+            frist_try++;
+        } while (num_of_operation != 5);
+    }
+}
